230832d28855d795073b755372deda94cfad2a45
Alexander Prokopev <avalarin@gmail.com>
2017-04-16 02:28:35 +0300

    add EnumerableExtensions: ForEach and JoinToString

---
+++ Teleglib/src/Utils/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teleglib.Utils {
+    public static class EnumerableExtensions {
+        public static void ForEach<T>(this IEnumerable<T> src, Action<T> supplier) {
+            foreach (var item in src) {
+                supplier(item);
+            }
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> src, Action<int, T> supplier) {
+            var i = 0;
+            foreach (var item in src) {
+                supplier(i++, item);
+            }
+        }
+
+        public static string JoinToString<T>(this IEnumerable<T> src, string separator) {
+            return string.Join(separator, src);
+        }
+    }
+}
